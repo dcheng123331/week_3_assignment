5dcc208d959cd60a48f7a605ad973c46d2e3c374
dcheng34551 <dcheng34551@gmail.com>
2021-03-14 01:47:01 +0800

    clean unused code

--- index.js
+++ index.js
@@ -1,36 +1,25 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
 app.use(express.static('public'));
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, './views'));
-
-const data = {result: 0};
 
 app.get('/getData', (req, res) => {
     const {number} = req.query;
 
     if (number === undefined) {
-        res.send('Lack of Parameter.');
+        res.send('Lack of Parameter');
     } else {
         let sum = 0;
         for (let i = 1; i <= number; i++) {
             sum += i;
         }
         if (sum === 0) {
-            res.render('wrongParam');
+            res.send('Wrong Parameter');
         } else {
-            data.result = sum;
-            res.render('rightParam', {data});
+            res.send(`<h1>${sum}</h1>`)
         }
     }
 });
-
-app.get('/sum.html', (req, res) => {
-    res.render('./public/sum.html', {data})
-})
 
 app.listen(3000, () => {
     console.log('Listening to post 3000')
