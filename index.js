7a37b1a2c851059f2d2b538d1c74b490046b33bd
dcheng34551 <dcheng34551@gmail.com>
2021-03-15 01:23:50 +0800

    finished all the assignment

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/getData', (req, res) => {
     const {number} = req.query;
@@ -19,6 +28,25 @@ app.get('/getData', (req, res) => {
             res.send(`<h1>${sum}</h1>`)
         }
     }
+});
+
+app.get('/myName', (req, res) => {
+    const name = req.cookies.name;
+    if(name) {
+        res.render('myName', {name});
+    } else {
+        res.redirect('/trackName')
+    }
+
+})
+
+app.get('/trackName', (req, res) => {
+    res.render('user');
+});
+
+app.post('/trackName', (req, res) => {
+    res.cookie('name', req.body.name)
+    res.redirect('/myName');
 });
 
 app.listen(3000, () => {
